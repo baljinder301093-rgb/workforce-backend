904f48944a9f4799f1020b12f0807d01ffdf1c74
baljinder301093-rgb <baljinder301093@gmail.com>
2026-06-08 09:16:06 -0700

    Delete lib directory

--- lib/db/src/schema/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./users";
-export * from "./projects";
-export * from "./applications";
-export * from "./assignments";
-export * from "./payments";
-export * from "./messages";
-export * from "./notifications";
